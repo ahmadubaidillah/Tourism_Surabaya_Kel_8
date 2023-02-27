87e4112404553592b2a8c458ddf3e674530b10f7
AHMAD <aua321890@gmail.com>
2023-02-27 11:45:10 +0700

    register done

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use App\Models\User;
+
 
 class LoginController extends Controller
 {
@@ -19,7 +22,15 @@ class LoginController extends Controller
     
     public function registeruser(Request $request)
     {
-        dd($request->all());
+        // dd($request->all());
+        User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>bcrypt($request->password),
+            'remember_token'=>Str::random(60),
+        ]);
+
+        return\redirect('/login');
     }
 
     /**
