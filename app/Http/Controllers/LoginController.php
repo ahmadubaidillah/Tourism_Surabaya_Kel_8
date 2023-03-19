acc12bacc586609965984210acece529c703c5dc
AHMAD <aua321890@gmail.com>
2023-03-19 11:38:37 +0700

    return

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -46,7 +46,7 @@ class LoginController extends Controller
     public function logout()
     {
         Auth::logout();
-        return \redirect('login');
+        return redirect('login');
     }
     
     /**
